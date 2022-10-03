9e8ec9814f0f963ade164820407f822a602d7cd9
YunsGeek <younes.12vfvf@gmail.com>
2022-10-03 20:23:09 +0100

    changes

--- controllers/reciters.js
+++ controllers/reciters.js
@@ -1,10 +1,12 @@
-import data from "../data/reciters.json" assert { type: "json" };
 import fs from "fs";
 
 // get all reciters
 export async function getReciters(req, res) {
   try {
-    res.status(200).json(data);
+    const file = JSON.parse(
+      fs.readFileSync(new URL("../data/reciters.json", import.meta.url))
+    );
+    res.status(200).json(file);
   } catch (error) {
     res.status(500).json({ success: false, error });
   }
